a5114a71822f2297b08d46bea382afe367c9a1cb
Jan Nawrocki <jan.nawrocki06@gmail.com>
2024-12-07 15:07:51 +0100

    refactor day6.go

--- src/days/day6.go
+++ src/days/day6.go
@@ -14,7 +14,7 @@ func main() {
 
 	var start Position
 	grid := make([][]int, len(lines))
-	traversed := make(map[Position][]int)
+	traversed := make(map[Position][]Direction)
 
 	for row, line := range lines {
 		grid[row] = make([]int, len(line))
@@ -38,30 +38,32 @@ func main() {
 	fmt.Println("Part2:", part2)
 }
 
-func part1(grid [][]int, start Position, traversed map[Position][]int) int {
+func part1(grid [][]int, start Position, traversed map[Position][]Direction) int {
 	var current, next Position
-	direction := 0
+	direction := Direction{0, -1}
 	current = start
 	for current.x > 0 && current.x < len(grid[0])-1 && current.y > 0 && current.y < len(grid)-1 {
-		traversed[current] = append(traversed[current], direction)
-		next.x = current.x + Directions[direction].x
-		next.y = current.y + Directions[direction].y
+		traversed[current] = append(traversed[current], Direction{direction.x, direction.y})
+		next = Position{
+			x: current.x + direction.x,
+			y: current.y + direction.y,
+		}
 
 		switch grid[next.y][next.x] {
 		case 0:
 			current = next
 			continue
 		case 1:
-			direction = (direction + 1) % 4
-			next.x = current.x + Directions[direction].x
-			next.y = current.y + Directions[direction].y
+			direction.rotate_90_degrees()
+			next.x = current.x + direction.x
+			next.y = current.y + direction.y
 			current = next
 		}
 	}
 	return len(traversed) + 1
 }
 
-func part2(grid [][]int, start Position, traversed map[Position][]int) int {
+func part2(grid [][]int, start Position, traversed map[Position][]Direction) int {
 	var count int
 	wg := sync.WaitGroup{}
 
@@ -97,11 +99,12 @@ type Position struct {
 	x, y int
 }
 
-var Directions = []struct{ x, y int }{
-	{0, -1},
-	{1, 0},
-	{0, 1},
-	{-1, 0},
+type Direction struct {
+	x, y int
+}
+
+func (d *Direction) rotate_90_degrees() {
+	d.x, d.y = -d.y, d.x
 }
 
 func looped(p, start Position, grid [][]int) bool {
@@ -112,33 +115,29 @@ func looped(p, start Position, grid [][]int) bool {
 	}
 	gridVariant[p.y][p.x] = 1
 	var current, next Position
-	traversed := make(map[Position][]int)
+	traversed := make(map[Position][]Direction)
 	current = start
-	direction := 0
+	direction := Direction{0, -1}
 
 	for current.x > 0 && current.x < len(gridVariant[0])-1 && current.y > 0 && current.y < len(gridVariant)-1 {
-		if traversed[current] == nil {
-			traversed[current] = []int{}
-		}
-		traversed[current] = append(traversed[current], direction)
+		traversed[current] = append(traversed[current], Direction{direction.x, direction.y})
 		next = Position{
-			x: current.x + Directions[direction].x,
-			y: current.y + Directions[direction].y,
+			x: current.x + direction.x,
+			y: current.y + direction.y,
 		}
 
-		if gridVariant[next.y][next.x] == 0 {
+		switch gridVariant[next.y][next.x] {
+		case 0:
 			if slices.Contains(
 				traversed[Position{next.x, next.y}],
-				direction,
+				Direction{direction.x, direction.y},
 			) {
-				grid[p.y][p.x] = 0
 				return true
 			}
 			current = next
-		} else if gridVariant[next.y][next.x] == 1 {
-			direction = (direction + 1) % 4
+		case 1:
+			direction.rotate_90_degrees()
 		}
 	}
-	gridVariant[p.y][p.x] = 0
 	return false
 }
